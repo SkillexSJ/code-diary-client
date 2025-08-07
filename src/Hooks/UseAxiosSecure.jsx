b0665de229f2a9cef2e3f0c0a62a0872608c4ba3
SKillexSJ <sajid7271.sk@gmail.com>
2025-08-07 17:52:27 +0600

    backend url change

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from "react-router";
 import { AuthContext } from "../Provider/AuthContext";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+
   headers: { "Content-Type": "application/json" },
 });
 
